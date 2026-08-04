929c1ccfa11e9886458a73c6a590f1b1665d543b
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-08-04 17:32:16 +0530

    feat: add routing with react-router-dom; implement Signup and Login components, and create Home component

---
+++ frontend/src/components/Signup/Signup.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Signup() {
+    return (
+        <>
+    <div className="min-h-screen flex items-center justify-center">
+  <div className="w-full  max-w-md  p-6 rounded-lg shadow-md bg-gray-400">
+    <h1 className="text-3xl font-bold text-center text-white">
+      Sign Up
+    </h1>
+  </div>
+</div>
+        </>
+    )
+}
+
+export default Signup
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Signup from './components/Signup/Signup.jsx'
+import Login from './components/Login/Login.jsx'
+import Home from './components/Home/Home.jsx'
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home/>
+    },
+    {
+      path: "/register",
+      element: <Signup/>
+    },
+    {
+      path: "/login",
+      element: <Login/>
+    },
+  ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router = {router} />
   </StrictMode>,
 )
