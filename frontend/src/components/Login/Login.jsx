aa43fd5d461458d6d62b9f45550778201fef110d
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-08-05 17:26:49 +0530

    feat: implement form handling and state management in Login and Signup components

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,75 +1,93 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, {useState} from 'react'
+import { Link } from "react-router-dom"
 
 function Login() {
-    return (
-               <>
-    <div className="min-h-screen flex items-center justify-center ">
-  <div className="w-full max-w-md  p-6 rounded-lg shadow-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
-    <h1 className="text-3xl font-bold text-center text-white">
-      Log In
-    </h1>
-       <form className="space-y-4">
 
-          {/* Username */}
-          <div>
-            <label className="block text-sm font-medium  text-white mb-1">
-              Username
-            </label>
-            <input
-              type="text"
-              placeholder="Enter your username or email"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+  const [user, setUser] = useState({
+    identifier: "",
+    password: "",
+  })
 
-          {/* Password */}
-          <div>
-            <label className="block text-sm font-medium  text-white mb-1">
-              Password
-            </label>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+  const onChange = (e) => {
+    const { name, value } = e.target
+    setUser((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  };
 
-          {/* Gender */}
-          <div>
-            <label className="block text-sm font-medium text-white mb-1">
-              Gender
-            </label>
-            <select
-              className="w-full  px-4 py-2 border border-gray rounded-lg  border-gray-100  focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              <option className='text-black ' value="">Select Gender</option>
-              <option className='text-black ' value="male">Male</option>
-              <option className='text-black ' value="female">Female</option>
-            </select>
-          </div>
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    console.log(user)
+    setUser({
+      identifier: "",
+      password: "",
+    })
+  }
 
-          {/* Signup Button */}
-          <button
-            type="submit"
-            className="w-full bg-blue-600 text-white py-2.5 rounded-lg font-semibold hover:bg-blue-700 transition duration-300"
-          >
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center ">
+        <div className="w-full max-w-md  p-6 rounded-lg shadow-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
+          <h1 className="text-3xl font-bold text-center text-white">
             Log In
-          </button>
+          </h1>
+          <form
+           onSubmit={onSubmitHandler}
+           className="space-y-4">
 
-          {/* Singup Link */}
-          <p className="text-center text-sm text-white">
-            Don't have an account?{" "}
-            <Link to= "/register" className="text-blue-600 hover:underline font-medium">
-            Sign up
-            </Link>
-          
-          </p>
-        </form>
-  </div>
-</div>
-        </>
-    )
+            {/* Username or email --> identifier */}
+            <div>
+              <label className="block text-sm font-medium  text-white mb-1">
+                Username/Email
+              </label>
+              <input
+                onChange={onChange}
+                name='identifier'
+                value={user.identifier}
+                type="text"
+                placeholder="Enter your username or email"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            {/* Password */}
+            <div>
+              <label className="block text-sm font-medium  text-white mb-1">
+                Password
+              </label>
+              <input
+                onChange={onChange}
+                name='password'
+                value={user.password}
+                type="password"
+                placeholder="Enter your password"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            {/* Login Button */}
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white py-2.5 rounded-lg font-semibold hover:bg-blue-700 transition duration-300"
+            >
+              Log In
+            </button>
+
+            {/* SignUp Link */}
+            <p className="text-center text-sm text-white">
+              Don't have an account?{" "}
+              <Link to="/register" className="text-blue-600 hover:underline font-medium">
+                Sign up
+              </Link>
+
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Login
--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -1,97 +1,144 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, { useState } from 'react'
+import { Link } from "react-router-dom"
 function Signup() {
-    return (
-        <>
-    <div className="min-h-screen flex items-center justify-center ">
-  <div className="w-full max-w-md  p-6 rounded-lg shadow-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
-    <h1 className="text-3xl font-bold text-center text-white">
-      Sign Up
-    </h1>
-       <form className="space-y-4">
-          {/* Full Name */}
-          <div>
-            <label className="block text-sm font-medium text-white mb-1">
-              Full Name
-            </label>
-            <input
-              type="text"
-              placeholder="Enter your full name"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
 
-          {/* Username */}
-          <div>
-            <label className="block text-sm font-medium  text-white mb-1">
-              Username
-            </label>
-            <input
-              type="text"
-              placeholder="Enter your username"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+  const [user, setUser] = useState({
+    fullName: "",
+    username: "",
+    email: "",
+    password: "",
+    gender: ""
+  })
 
-          {/* Email */}
-          <div>
-            <label className="block text-sm font-medium  text-white mb-1">
-              Email
-            </label>
-            <input
-              type="email"
-              placeholder="Enter your email"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+  const onChange = (e) => {
+    const { name, value } = e.target
+    setUser((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  };
 
-          {/* Password */}
-          <div>
-            <label className="block text-sm font-medium  text-white mb-1">
-              Password
-            </label>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    console.log(user)
+    setUser({
+      fullName: "",
+      username: "",
+      email: "",
+      password: "",
+      gender: ""
+    })
+  }
 
-          {/* Gender */}
-          <div>
-            <label className="block text-sm font-medium text-white mb-1">
-              Gender
-            </label>
-            <select
-              className="w-full  px-4 py-2 border border-gray rounded-lg  border-gray-100  focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              <option  className='text-black ' value="">Select Gender</option>
-              <option className='text-black ' value="male">Male</option>
-              <option className='text-black ' value="female">Female</option>
-            </select>
-          </div>
 
-          {/* Signup Button */}
-          <button
-            type="submit"
-            className="w-full bg-blue-600 text-white py-2.5 rounded-lg font-semibold hover:bg-blue-700 transition duration-300"
-          >
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center ">
+        <div className="w-full max-w-md  p-6 rounded-lg shadow-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
+          <h1 className="text-3xl font-bold text-center text-white">
             Sign Up
-          </button>
+          </h1>
+          <form
+            onSubmit={onSubmitHandler}
+            className="space-y-4">
+            {/* Full Name */}
+            <div>
+              <label className="block text-sm font-medium text-white mb-1">
+                Full Name
+              </label>
+              <input
+                onChange={onChange}
+                name='fullName'
+                value={user.fullName}
+                type="text"
+                placeholder="Enter your full name"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
 
-          {/* Login Link */}
-          <p className="text-center text-sm text-white">
-            Already have an account?{" "}
-            <Link to= "/login" className="text-blue-600 hover:underline font-medium">
-            Login
-            </Link>
-          
-          </p>
-        </form>
-  </div>
-</div>
-        </>
-    )
+            {/* Username */}
+            <div>
+              <label className="block text-sm font-medium  text-white mb-1">
+                Username
+              </label>
+              <input
+                onChange={onChange}
+                name='username'
+                value={user.username}
+                type="text"
+                placeholder="Enter your username"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            {/* Email */}
+            <div>
+              <label className="block text-sm font-medium  text-white mb-1">
+                Email
+              </label>
+              <input
+                onChange={onChange}
+                name='email'
+                value={user.email}
+                type="email"
+                placeholder="Enter your email"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            {/* Password */}
+            <div>
+              <label className="block text-sm font-medium  text-white mb-1">
+                Password
+              </label>
+              <input
+                onChange={onChange}
+                name='password'
+                value={user.password}
+                type="password"
+                placeholder="Enter your password"
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+
+            {/* Gender */}
+            <div>
+              <label className="block text-sm font-medium text-white mb-1">
+                Gender
+              </label>
+              <select
+                onChange={onChange}
+                name='gender'
+                value={user.gender}
+                className="w-full  px-4 py-2 border border-gray rounded-lg  border-gray-100  focus:outline-none focus:ring-2 focus:ring-blue-500"
+              >
+                <option className='text-black ' value="">Select Gender</option>
+                <option className='text-black ' value="male">Male</option>
+                <option className='text-black ' value="female">Female</option>
+              </select>
+            </div>
+
+            {/* Signup Button */}
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white py-2.5 rounded-lg font-semibold hover:bg-blue-700 transition duration-300"
+            >
+              Sign Up
+            </button>
+
+            {/* Login Link */}
+            <p className="text-center text-sm text-white">
+              Already have an account?{" "}
+              <Link to="/login" className="text-blue-600 hover:underline font-medium">
+                Login
+              </Link>
+
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Signup
