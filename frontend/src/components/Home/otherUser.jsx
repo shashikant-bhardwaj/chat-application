1a619c6f972c74d440019a9c4e445f70b8bccced
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-08-10 19:37:57 +0530

    feat: remove online status indicator from user component to streamline UI

--- frontend/src/components/Home/otherUser.jsx
+++ frontend/src/components/Home/otherUser.jsx
@@ -35,10 +35,7 @@ function OtherUser({ user }) {
         alt=""
         className="w-12 h-12 rounded-full"
       />
-         {isOnline && (
-        <span className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 rounded-full border-2 border-gray-900"></span>
-    )}
-
+        
       <div>
 
         <h2 className="text-white font-semibold">
